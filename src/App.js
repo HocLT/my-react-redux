5ecc8611181abbad90f3e1378a1c6b3cb92fcf9d
HocLT <hdquang6@gmail.com>
2022-06-29 17:57:43 +0700

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
 import './style.css';
 
 export default function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
-        <Route path="/login"></Route>
+        <Route path="/login" element={<Login />} />
         <Route path="/dashboard"></Route>
-        <Route path="*"></Route>
+        <Route path="*" element={<Login />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,14 +2,23 @@ import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 export default function Login() {
-  const [username, setUsername] = useState("");
-  const history = useHistory();
+  const [username, setUsername] = useState('');
+  const history = useHistory;
 
   function handleLogin() {
     history.push('/dashboard');
   }
 
   return (
-    
+    <>
+      <h1>Login</h1>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(evt) => setUsername(evt.target.value)}
+      />{' '}
+      <button onClick={handleLogin}>Login</button>
+    </>
   );
 }
